1f63e0deacf1783f4b1388b4f1c0b2dc90a20e9f
Lanka <darshaka4mail@gmial.com>
2016-11-14 12:22:48 +0530

    react Config

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = require('./component/App.jsx');
+
+ReactDOM.render(<App title="React App" />,document.getElementById('app'));
